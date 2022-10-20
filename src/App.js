4a38091f8bb04855bf8687133c9c1aa2aa73b3eb
LPandorf <leticiapandorf@gmail.com>
2022-10-20 15:35:40 -0300

    importação das fontes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import GlobalStyle from "./assets/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Login from "./telas/Login";
-import Cadastro from "./telas/Cadastro";
+//import Login from "./telas/Login";
+//import Cadastro from "./telas/Cadastro";
 import Habitos from "./telas/Habitos";
-import Historico from "./telas/Historico";
-import Hoje from "./telas/Hoje";
+//import Historico from "./telas/Historico";
+//import Hoje from "./telas/Hoje";
 
 export default function App(){
     return (
@@ -13,14 +13,18 @@ export default function App(){
             <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/hoje" element={<Hoje />} />
-                    <Route path="/habitos" element={<Habitos />} />
-                    <Route path="/historico" element={<Historico />} />
-                    <Route path="/cadastro" element={<Cadastro />} />
-
+                    <Route path="/" element={<Habitos />} />
                 </Routes>
             </BrowserRouter>
         </>
     )
 }
+
+
+/*
+<Route path="/" element={<Login />} />
+<Route path="/hoje" element={<Hoje />} />
+habitos
+<Route path="/historico" element={<Historico />} />
+<Route path="/cadastro" element={<Cadastro />} />
+*/
--- src/telas/Habitos.js
+++ src/telas/Habitos.js
@@ -1,4 +1,4 @@
-//import styled from "styled-components";
+import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -6,7 +6,41 @@ export default function Habitos(){
     return(
         <>
             <Navbar/>
+            <Corpo>
+                <Titulo>Meus hábitos</Titulo>
+                <Nenhum>Você não tem nenhum hábito cadastrado ainda. Adicione um para começar a trackear</Nenhum>
+            </Corpo>
             <Footer/>
         </>
     )
 }
+const Corpo=styled.div`
+    margin-top: 70px;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    background-color: #E5E5E5;
+    padding: 20px;
+    `
+
+const Titulo= styled.div`
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126BA5;
+    margin-bottom: 20px;
+`
+
+const Nenhum= styled.div`
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17.976px;
+    line-height: 22px;
+    color: #666666;
+`
