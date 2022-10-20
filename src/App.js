7ca2a487d4c33bddf83cada0f4290665372358fd
LPandorf <leticiapandorf@gmail.com>
2022-10-20 14:42:48 -0300

    app routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-//import { useState } from "react"
 import GlobalStyle from "./assets/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-//import Login from "./telas/Login";
-//import Cadastro from "./telas/Cadastro";
-//import Habitos from "./telas/Habitos";
-//import Historico from "./telas/Historico";
-//import Hoje from "./telas/Hoje";
+import Login from "./telas/Login";
+import Cadastro from "./telas/Cadastro";
+import Habitos from "./telas/Habitos";
+import Historico from "./telas/Historico";
+import Hoje from "./telas/Hoje";
 
 export default function App(){
     return (
         <>
-            <GlobalStyle/>
+            <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                        
+                    <Route path="/" element={<Login />} />
+                    <Route path="/hoje" element={<Hoje />} />
+                    <Route path="/habitos" element={<Habitos />} />
+                    <Route path="/historico" element={<Historico />} />
+                    <Route path="/cadastro" element={<Cadastro />} />
+
                 </Routes>
             </BrowserRouter>
         </>
     )
 }
-
-/*
-<Route path="/hoje" element={<Hoje/>}/>
-<Route path="/habitos" element={<Habitos/>}/>
-<Route path="/historico" element={<Historico/>}/>
-<Route path="/" element={<Login/>}/>
-<Route path="/cadastro" element={<Cadastro/>}/>
-
-*/
